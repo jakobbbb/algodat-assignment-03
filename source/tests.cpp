0eb0bd27149aaeca6c022aebadf9e97df350317b
Jakob Ruckel <jakob@ruckel.de>
2020-06-25 13:38:55 +0200

    tests: reflect 8b182708661de2516efa7719989a109a51a52560

--- source/tests.cpp
+++ source/tests.cpp
@@ -4,27 +4,6 @@
 #include <vector>
 #include "graph.hpp"
 
-SCENARIO("min-heap", "[heap]") {
-  std::srand(0);
-
-  MinHeap h;
-  REQUIRE(h.empty());
-  REQUIRE(0 == h.size());
-
-  for (std::size_t i = 0; i < 100; ++i) {
-    auto n = new Node{"n" + std::to_string(i)};
-    n->key = std::rand() % 20;
-    h.add(n);
-    REQUIRE(!h.empty());
-    REQUIRE(h.valid());
-    REQUIRE(i + 1 == h.size());
-  }
-  for (std::size_t i = 0; i < 100; ++i) {
-    h.extract_smallest();
-    REQUIRE(h.valid());
-  }
-}
-
 /**
  * Helper function to generate a graph of size `n_elements`, where each pair of
  * elements has a chance `p` of being connected with a random weight between 1
@@ -132,5 +111,46 @@ SCENARIO("bellman-ford", "[bellman]") {
     auto [g, nodes] = make_graph(true, size, 15, 0.1f);
     REQUIRE(size == g->size());
     REQUIRE_NOTHROW(g->bellmann_ford(nodes.front()));
+  }
+}
+
+SCENARIO("prim", "[prim]") {
+  GIVEN("the digraph presented in the exercise") {
+    Graph g{true};
+
+    auto a = g.add({"a"});
+    auto b = g.add({"b"});
+    auto c = g.add({"c"});
+    auto d = g.add({"d"});
+    auto e = g.add({"e"});
+    auto f = g.add({"f"});
+
+    a->connect(e, 7);
+    a->connect(f, 5);
+    b->connect(a, 1);
+    b->connect(c, 3);
+    c->connect(d, 9);
+    d->connect(c, 3);
+    d->connect(e, 2);
+    d->connect(f, 1);
+    e->connect(b, 2);
+    e->connect(c, 4);
+    f->connect(e, 2);
+    REQUIRE_NOTHROW(g.prim());
+  }
+
+  GIVEN("a digraph") {
+    const int size = 20;
+    auto [g, nodes] = make_graph(true, size, 15, 0.1f);
+    REQUIRE(size == g->size());
+    REQUIRE_NOTHROW(g->prim());
+    REQUIRE_NOTHROW(g->print(std::cout));
+  }
+
+  GIVEN("a large digraph") {
+    const int size = 200;
+    auto [g, nodes] = make_graph(true, size, 15, 0.1f);
+    REQUIRE_NOTHROW(g->prim());
+    REQUIRE_NOTHROW(g->print(std::cout));
   }
 }
